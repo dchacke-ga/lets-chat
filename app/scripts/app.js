09b33a536b42aa76caa585200501a98b43f9e328
Dennis Charles Hackethal <dennis@generalassemb.ly>
2014-12-16 15:40:48 -0800

    Prepare app for usage with Firebase

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,27 +8,29 @@
  *
  * Main module of the application.
  */
-angular
-  .module('gaChatApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch',
-    'firebase'
-  ])
-  .config(function ($routeProvider) {
-    // $routeProvider
-      // .when('/', {
-      //   templateUrl: 'views/main.html',
-      //   controller: 'MainCtrl'
-      // })
-      // .when('/about', {
-      //   templateUrl: 'views/about.html',
-      //   controller: 'AboutCtrl'
-      // })
-      // .otherwise({
-      //   redirectTo: '/'
-      // });
-  });
+app = angular.module('gaChatApp', [
+  'ngAnimate',
+  'ngCookies',
+  'ngResource',
+  'ngRoute',
+  'ngSanitize',
+  'ngTouch',
+  'firebase'
+]);
+
+app.constant('FIREBASE_URL', 'https://ga-chat.firebaseio.com/');
+
+app.config(function ($routeProvider) {
+  // $routeProvider
+    // .when('/', {
+    //   templateUrl: 'views/main.html',
+    //   controller: 'MainCtrl'
+    // })
+    // .when('/about', {
+    //   templateUrl: 'views/about.html',
+    //   controller: 'AboutCtrl'
+    // })
+    // .otherwise({
+    //   redirectTo: '/'
+    // });
+});
