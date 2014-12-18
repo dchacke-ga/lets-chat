1ff039fdf3517cbcd51b1e87ccba855b33384db2
Dennis Charles Hackethal <dennis@generalassemb.ly>
2014-12-17 17:00:50 -0800

    Implement messages

--- app/scripts/controllers/roomsCtrl.js
+++ app/scripts/controllers/roomsCtrl.js
@@ -1,23 +1,29 @@
 'use strict';
 
-app.controller('RoomsCtrl', function($scope, $location, $routeParams, Auth, Room, user) {
+app.controller('RoomsCtrl', function($scope, $location, $routeParams, Auth, Room, Message, user) {
 	if (!user) {
 		$location.path('/sessions/new');
 	}
 
-	// index
+	// #index
 	$scope.rooms = Room.all;
 
-	// show
+	// #show
 	if ($routeParams.id) {
-		$scope.room = Room.get($routeParams.id);
+		var id = $routeParams.id;
+		$scope.room = Room.get(id);
+		$scope.messages = Message.all(id);
 
 		$scope.sendMessage = function() {
-			console.log('Add logic to send message here...');
+			console.log('Sending message');
+			$scope.message.sender = Auth.user.profile.username;
+			Message.create(id, $scope.message).then(function() {
+				$scope.message.body = '';
+			});
 		};
 	}
 
-	// new
+	// #create
 	$scope.create = function() {
 		Room.create($scope.room).then(function(room) {
 			$location.path('/rooms/' + room.key());
---
+++ app/scripts/services/message.js
@@ -0,0 +1,21 @@
+'use strict';
+
+app.factory('Message', function($firebase, FIREBASE_URL) {
+	var ref = new Firebase(FIREBASE_URL);
+	var messages = function(roomId) {
+		return $firebase(ref.child('messages').child(roomId)).$asArray()
+	};
+
+	var Message = {
+		all: function(roomId) {
+			return messages(roomId);
+		},
+		create: function(roomId, message) {
+			return messages(roomId).$add(message).then(function(messageRef) {
+				return messageRef;
+			});
+		}
+	};
+
+	return Message;
+});
